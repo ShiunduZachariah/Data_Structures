7a5453449061f89a1a143ab49c24a6ce78b0392b
aikonkali <brianzion63@gmail.com>
2022-10-27 17:34:20 +0300

    Update

---
+++ Integer Reversion.py
@@ -0,0 +1,16 @@
+# Reverse Intergers
+
+def reverse_integer(n):
+    reversed_integer = 0
+    remainder = 0
+
+    while n > 0:
+        remainder = n % 10
+        reversed_integer = reversed_integer * 10 + remainder
+        n = n // 10
+
+    return reversed_integer
+
+
+if __name__ == '__main__':
+    print(reverse_integer(1234))
